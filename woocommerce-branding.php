22aa809048b928bb69c98eb9275f7b15a04c665c
romeo.covaci@gmail.com <romeo.covaci@gmail.com>
2019-11-08 13:20:12 +0200

    Update to 1.0.21

--- woocommerce-branding.php
+++ woocommerce-branding.php
@@ -3,16 +3,16 @@
  * Plugin Name: HYP WC Branding
  * Plugin URI: https://github.com/hypericumimpex/hyp-wc-branding/
  * Description: Rebrand UI.
- * Version: 1.0.19
+ * Version: 1.0.21
  * Author: Hypericum
  * Author URI: https://github.com/hypericumimpex/
  * Requires at least: 3.1
  * Tested up to: 4.8
  * Woo: 19003:b57eb3de77456cf73ef6f7456a03ea83
- * WC tested up to: 3.6
+ * WC tested up to: 3.8
  * WC requires at least: 2.6
  *
- * Copyright: © 2009-2017 WooCommerce.
+ * Copyright: © 2009-2019 Hypericum
  * License: GNU General Public License v3.0
  * License URI: http://www.gnu.org/licenses/gpl-3.0.html
  */
